61f251b366f1c28747920339dc0c929cb5e709e2
Min Idznullah Said <minidznullahsaid@gmail.com>
2025-09-18 11:19:39 +0700

    Yacth section created

---
+++ UsrYacht/Schemas/UsrYacth_Detail/UsrYacth_Detail.js
@@ -0,0 +1,8 @@
+define('UsrYacth_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrYacth',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
